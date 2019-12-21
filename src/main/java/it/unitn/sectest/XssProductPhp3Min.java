6f10ecdcb4bce0b102b022092497145691f7a5f3
ainter21 <alberto.giust@studenti.unitn.it>
2019-12-21 12:25:19 +0100

    Finish product page

---
+++ src/main/java/it/unitn/sectest/XssProductPhp3Min.java
@@ -0,0 +1,50 @@
+package it.unitn.sectest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Test;
+
+import po.BrandPage;
+import po.DashBoardPage;
+import po.ProductPage;
+import utils.BaseTest;
+
+public class XssProductPhp3Min extends BaseTest{
+
+	
+	DashBoardPage dashboardPage;
+	BrandPage brandPage;
+	ProductPage productPage;
+	@Test
+	public void test() {
+
+		dashboardPage = defaultLogin();
+		waitForWebsite();
+		brandPage = dashboardPage.goToBrandPage();
+		brandPage.addBrand("Malicious<script>alert(\"brand\")</script>", "Available");
+		waitForWebsite();
+		productPage = brandPage.goToProductPage();
+		productPage.clickAlert();
+		waitForWebsite();
+		productPage.clickAlert();
+		assertTrue(productPage.isAlert());
+		
+		
+		
+	}
+	
+	@After
+	public void reset() {
+		
+		waitForWebsite();
+		brandPage = productPage.goToBrandPage();
+		brandPage.clickAlert();
+		waitForWebsite();
+		brandPage.removeBrand();
+		waitForWebsite();
+		productPage.logout();
+		
+	}
+
+}
---
+++ src/main/java/it/unitn/sectest/XssProductPhp4Min.java
@@ -0,0 +1,53 @@
+package it.unitn.sectest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Test;
+
+import po.CategoriesPage;
+import po.DashBoardPage;
+import po.ProductPage;
+import utils.BaseTest;
+
+public class XssProductPhp4Min extends BaseTest{
+
+	DashBoardPage dashboardPage;
+	CategoriesPage categoriesPage;
+	ProductPage productPage;
+	@Test
+	public void test() {
+		
+		dashboardPage = defaultLogin();
+		waitForWebsite();
+		
+		categoriesPage = dashboardPage.goToCategoriesPage();
+		
+		categoriesPage.addCategory("Malicious<script>alert(\"categories\")</script>", "Available");
+		
+		waitForWebsite();
+		
+		productPage = categoriesPage.goToProductPage();
+		productPage.clickAlert();
+		waitForWebsite();
+		productPage.clickAlert();
+		waitForWebsite();
+		
+		assertTrue(productPage.isAlert());
+		
+	}
+	
+	@After
+	public void reset() {
+	
+		categoriesPage = productPage.goToCategoriesPage();
+		waitForWebsite();
+		categoriesPage.clickAlert();
+		waitForWebsite();
+		categoriesPage.removeCategory();
+		waitForWebsite();
+		categoriesPage.logout();
+		
+	}
+
+}
